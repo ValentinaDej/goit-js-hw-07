2b234eda558bdff6d99c53dd3da8e03db740338b
ValentinaDej <safonikv@gail.com>
2022-10-30 23:12:06 +0200

    data

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,21 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
+const markup = galleryItems
+  .map(
+    (galleryItem) => `<div class="gallery__item">
+<a class="gallery__link" href="${galleryItem.original}">
+     <img
+       class="gallery__image"
+       src="${galleryItem.preview}"
+       data-source="${galleryItem.original}"
+       alt="${galleryItem.description}"
+     />
+   </a>
+ </div>`
+  )
+  .join("");
+
+const list = document.querySelector(".gallery");
+list.insertAdjacentHTML("beforeend", markup);
 console.log(galleryItems);
