1ab9cc86465a196f5aaf10fc753ce49417098df3
ValentinaDej <safonikv@gail.com>
2022-11-01 01:37:05 +0200

    galery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -16,6 +16,40 @@ const markup = galleryItems
   )
   .join("");
 
-const list = document.querySelector(".gallery");
-list.insertAdjacentHTML("beforeend", markup);
-console.log(galleryItems);
+const galleryItem = document.querySelector(".gallery");
+
+galleryItem.insertAdjacentHTML("beforeend", markup);
+
+galleryItem.addEventListener("click", hanleImgClick);
+
+function hanleImgClick(e) {
+  e.preventDefault();
+  // if (!e.target.classList.contains("gallery__image")) {
+  //   return;
+  // }
+  if (!e.target.dataset.source) {
+    return;
+  }
+
+  const originalImg = e.target.dataset.source;
+  createLightBox(originalImg).show();
+
+  document.body.style.overflowY = "hidden";
+  document.style.overflowY = "hidden";
+}
+
+const createLightBox = (originalImg) => {
+  const instance = basicLightbox.create(`
+    <img src="${originalImg}">
+`);
+  return instance;
+};
+
+document.body.onkeydown = function (e) {
+  e = e || window.event;
+  var c = e.keyCode;
+  //Убирает эвент на стрелках, на pageDown, PageUp, Home, End
+  if ((c > 36 && c < 41) || (c > 32 && c < 37)) {
+    return false;
+  }
+};
