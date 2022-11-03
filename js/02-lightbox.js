d4613b731145152c37bf7f84f132755a8ce38598
ValentinaDej <safonikv@gail.com>
2022-11-03 02:29:47 +0200

    2 createLitebox

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -15,39 +15,12 @@ const markupGalleryList = galleryItems
 
 galleryList.insertAdjacentHTML("beforeend", markupGalleryList);
 galleryList.addEventListener("click", onImgClick);
+const createLightbox = new SimpleLightbox(".gallery a", {});
 
 function onImgClick(e) {
   e.preventDefault();
   if (!e.target.classList.contains("gallery__image")) {
     return;
   }
-  createLightBox(e.target.dataset.source).show();
+  createLightbox();
 }
-
-let onActiveImg;
-
-const createLightBox = (original) => {
-  const instance = basicLightbox.create(
-    `
-    <img src="${original}">
-`,
-    {
-      onShow: () => {
-        onActiveImg = onKeyPress.bind(this, instance);
-        document.addEventListener("keydown", onActiveImg);
-        document.body.classList.add("scroll-disable");
-      },
-      onClose: () => {
-        document.removeEventListener("keydown", onActiveImg);
-        document.body.classList.remove("scroll-disable");
-      },
-    }
-  );
-  return instance;
-};
-
-const onKeyPress = function (instance) {
-  if (event.key === "Escape") {
-    instance.close();
-  }
-};
