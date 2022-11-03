138bd11b997dc0a29e8326b2111d281407e843da
ValentinaDej <safonikv@gail.com>
2022-11-03 02:16:19 +0200

    2 murkup

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,22 +1,15 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
-
 const galleryList = document.querySelector(".gallery");
 
 const markupGalleryList = galleryItems
   .map(
     ({ original, preview, description }) => `<div class="gallery__item"> 
-    <a class="gallery__link" href="${original}">
-     <img
-       class="gallery__image"
-       src="${preview}"
-       data-source="${original}"
-       alt="${description}"
-     />
-   </a>
- </div>`
+    <a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>
+    </div>`
   )
   .join("");
 
