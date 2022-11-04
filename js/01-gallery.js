279503b68b7531f42a190ea11f3dd55ae3aa9f25
ValentinaDej <safonikv@gail.com>
2022-11-04 23:28:14 +0200

    update

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -59,23 +59,27 @@ const onKeyPress = function (instance) {
   }
 };
 
-const lazyImg = document.querySelectorAll("img.gallery__image");
+lazyLoading();
 
-if ("loading" in HTMLImageElement.prototype) {
-  lazyImg.forEach((el) => el.setAttribute("loading", "lazy"));
-} else {
-  const script = document.createElement("script");
-  script.src =
-    "https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.3.2/lazysizes.min.js";
-  script.integrity =
-    "sha512-q583ppKrCRc7N5O0n2nzUiJ+suUv7Et1JGels4bXOaMFQcamPk9HjdUknZuuFjBNs7tsMuadge5k9RzdmO+1GQ==";
-  script.crossorigin = "anonymous";
-  script.referrerpolicy = "no-referrer";
+function lazyLoading() {
+  const lazyImg = document.querySelectorAll("img.gallery__image");
 
-  document.body.appendChild(script);
+  if ("loading" in HTMLImageElement.prototype) {
+    lazyImg.forEach((el) => el.setAttribute("loading", "lazy"));
+  } else {
+    const script = document.createElement("script");
+    script.src =
+      "https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.3.2/lazysizes.min.js";
+    script.integrity =
+      "sha512-q583ppKrCRc7N5O0n2nzUiJ+suUv7Et1JGels4bXOaMFQcamPk9HjdUknZuuFjBNs7tsMuadge5k9RzdmO+1GQ==";
+    script.crossorigin = "anonymous";
+    script.referrerpolicy = "no-referrer";
 
-  lazyImg.forEach((el) => {
-    el.setAttribute("data-src", el.src);
-    el.classList.add("lazyload");
-  });
+    document.body.appendChild(script);
+
+    lazyImg.forEach((el) => {
+      el.setAttribute("data-src", el.src);
+      el.classList.add("lazyload");
+    });
+  }
 }
